b360ea6d0d28bb9fdf078a1215492ce248453274
Adam <adam.henault@etu.univ-tours.fr>
2021-05-11 02:00:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,51 @@
-#include "SDL.h"
 #include <windows.h>
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include "SDL.h"
+#include "SDL_image.h"
 
 using namespace std;
 
-#define WINDOW_WIDTH 800
-
-
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 
+    bool quit = false;
     SDL_Event event;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    IMG_Init(IMG_INIT_JPG);
 
-    int frameRate = 60;
-    int frameDuration = 1000 / frameRate;
-    int t = 0;
-    while (true) {
-        auto t1 = std::chrono::high_resolution_clock::now();
+    SDL_Window * window = SDL_CreateWindow("Pokemon++",
+                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            break;
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface * image = IMG_Load("test.jpg");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
 
-        SDL_RenderDrawPoint(renderer, t%WINDOW_WIDTH, t/WINDOW_WIDTH);
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+
+        //SDL_Rect dstrect = { 5, 5, 320, 240 };
+        //SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
-        t++;
-
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-        if( duration < frameDuration )
-            std::this_thread::sleep_for(std::chrono::milliseconds(frameDuration-duration));
     }
 
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+
+    IMG_Quit();
     SDL_Quit();
-    return EXIT_SUCCESS;
+
+    return 0;
 }
