973e3a0da5dd50b6f9d2cc7d0ce329dbe85ead42
Adam <adam.henault@etu.univ-tours.fr>
2021-06-01 00:21:53 +0200

    Implementation of Texture Manager

--- Manager/Texture/TextureManager.cpp
+++ Manager/Texture/TextureManager.cpp
@@ -1,1 +1,69 @@
 #include "TextureManager.h"
+
+bool TextureManager::load(char *filePath, char *id, SDL_Renderer *pRenderer)
+{
+    SDL_Surface* pTempSurface = IMG_Load(filePath);
+
+    if (pTempSurface == nullptr)
+        return false;
+
+    SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
+    SDL_FreeSurface(pTempSurface);
+
+    if (pTexture != nullptr)
+    {
+        mTextureMap.at(id) = pTexture;
+        return true;
+    }
+
+    return false;
+}
+
+void TextureManager::draw(char *id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip)
+{
+    SDL_Rect srcRect;
+    SDL_Rect destRect;
+
+    srcRect.x = 0;
+    srcRect.y = 0;
+    srcRect.w = width;
+    srcRect.h = height;
+
+    destRect.x = x;
+    destRect.y = y;
+    destRect.w = width;
+    destRect.h = height;
+
+    SDL_RenderCopyEx(pRenderer, mTextureMap.at(id), &srcRect, &destRect, 0, nullptr, flip);
+}
+
+void TextureManager::drawFrame(char *id, int x, int y, int width, int height, int currentRow, int currentFrame,
+                               SDL_Renderer *pRenderer, SDL_RendererFlip flip)
+{
+    SDL_Rect srcRect;
+    SDL_Rect destRect;
+
+    srcRect.x = width * currentFrame;
+    srcRect.y =  height * (currentRow - 1);
+    srcRect.w = width;
+    srcRect.h = height;
+
+    destRect.x = x;
+    destRect.y = y;
+    destRect.w = width;
+    destRect.h = height;
+
+    SDL_RenderCopyEx(pRenderer, mTextureMap.at(id), &srcRect, &destRect, 0, nullptr, flip);
+}
+TextureManager *TextureManager::Instance()
+{
+    if (s_pInstance == nullptr)
+    {
+        s_pInstance = new TextureManager();
+        return s_pInstance;
+    }
+
+    return s_pInstance;
+}
+
+
--- Manager/Texture/TextureManager.h
+++ Manager/Texture/TextureManager.h
@@ -1,11 +1,23 @@
 #ifndef POKEMONPP_MANAGER_TEXTURE_TEXTUREMANAGER_H
 #define POKEMONPP_MANAGER_TEXTURE_TEXTUREMANAGER_H
 
+#include <map>
+#include "SDL.h"
+#include "SDL_image.h"
+
 class TextureManager
 {
-public:
-    bool load (char* filePath, char* id, SDL_Renderer* pRenderer);
+private:
+    static TextureManager *s_pInstance;
+    std::map<char*, SDL_Texture*> mTextureMap;
+    TextureManager()= default;
+    ~TextureManager()=default;
 
+public:
+    static TextureManager *Instance();
+    bool load (char *filePath, char *id, SDL_Renderer *pRenderer);
+    void draw (char *id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void drawFrame (char *id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
 };
 
 #endif //POKEMONPP_MANAGER_TEXTURE_TEXTUREMANAGER_H
